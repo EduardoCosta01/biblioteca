a0d8d16a7da65030f4e0013e7373d9d4a8fe87f0
Eduardo Costa01 <eccosta2000@bol.com.br>
2026-06-30 17:11:15 -0300

    primeiras rotas

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const OpcoesContainer = styled.ul`
@@ -17,13 +18,13 @@ const Opcao = styled.li`
 `
 
 
-const textoOpcoes = ['CATEGORIAS', 'MINHA ESTANTE',  'FAVORITOS']
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'ESTANTE']
 
 function OpcoesHeader() {
     return(
         <OpcoesContainer>
           { textoOpcoes.map((texto) => (
-            <Opcao><p>{texto}</p></Opcao>
+            <Link to={`/${texto.toLowerCase()}`}><Opcao><p>{texto}</p></Opcao></Link>
           ))}
         </OpcoesContainer>
     )
